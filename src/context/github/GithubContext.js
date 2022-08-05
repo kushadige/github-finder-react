900abee4c79fad6e8e0db01ade251d33e5cc0cfc
blitzkrieg <oguzhankuslar@gmail.com>
2022-08-05 18:36:52 +0300

    User search component, reducers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,32 +1,76 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
 export const GithubProvider = ({ children }) => {
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const initialState = {
+        users: [],
+        isLoading: false
+    }
 
-    useEffect(() => {
-        setIsLoading(true);
-        fetchUsers();
-    }, []);
+    const [state, dispatch] = useReducer(githubReducer, initialState);
 
+    // Get initial users (testing purposes)
     const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+        setLoading();
+
+        const response = await fetch(`${GITHUB_URL}/users`, {
             headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+                Authorization: `${GITHUB_TOKEN}`
             }
         });
 
         const data = await response.json();
 
-        setUsers(data);
-        setIsLoading(false);
+        dispatch({
+            type: 'GET_USERS',
+            payload: data
+        });
+    }
+
+    const clearUsers = () => {
+        dispatch({
+            type: 'CLEAR_USERS'
+        });
+    }
+
+    const searchUsers = async (text) => {
+        setLoading();
+
+        const params = new URLSearchParams({
+            q: text
+        });
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+            headers: {
+                Authorization: `{GITHUB_TOKEN}`
+            }
+        });
+        
+        const { items } = await response.json();
+
+        dispatch({
+            type: 'GET_USERS',
+            payload: items
+        });
+    }
+
+    const setLoading = () => {
+        dispatch({
+            type: 'SET_LOADING'
+        });
     }
 
     return <GithubContext.Provider value={{
-        users,
-        isLoading
+        users: state.users,
+        isLoading: state.isLoading,
+        fetchUsers,
+        clearUsers,
+        searchUsers
     }}>
         {children}
     </GithubContext.Provider>
